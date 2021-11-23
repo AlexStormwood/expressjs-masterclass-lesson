7864d030c84fb2c68fe307a04d86c8b119c0b7e3
Alex H <alex.holder@bigfootds.com>
2021-11-23 12:55:10 +1100

    Functions stuff

---
+++ src/Posts/postsFunctions.js
@@ -0,0 +1,14 @@
+
+
+function randomNumberGenerator(){
+
+    return 3;
+}
+
+async function someAsyncFunction(){
+    return "some async thing"
+}
+
+module.exports = {
+    randomNumberGenerator, someAsyncFunction
+}
--- src/Posts/postsRoutes.js
+++ src/Posts/postsRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { randomNumberGenerator } = require('./postsFunctions');
 
 const routes = express.Router();
 
@@ -8,6 +9,12 @@ routes.get('/', (request, response) => {
     response.json(`Received a request on ${request.originalUrl}`);
 
 });
+
+routes.get('/randomNumber',(request,response)=>{
+
+    response.send(`<h1>${randomNumberGenerator()}</h1>`); 
+ });
+ 
 
 routes.get('/:postID', (request, response) => {
 
@@ -26,12 +33,13 @@ routes.post('/:postID', (request, response) => {
 
     // for form urlencoded submission
     //let submittedPokemon = JSON.parse(request.body.favouritePokemon).name;
-    
+
     // for raw json submission
     let submittedPokemon = request.body.favouritePokemon.name;
 
     response.json(`Received fave Pokemon of ${submittedPokemon} `)
 });
+
 
 // routes.get('/:username/status/:postID', (request, response) => {
 
